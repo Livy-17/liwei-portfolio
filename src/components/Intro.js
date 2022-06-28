adf3a343dac8e5a22b4a4f53f73adcdc5b6f7e60
Livy <42135339+Livy-17@users.noreply.github.com>
2022-06-28 19:08:19 +0100

    commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Heading, Text, Box, Flex, Button} from "@chakra-ui/react";
+import { useColorModeValue, useMediaQuery } from "@chakra-ui/react";
 
 const Intro = props => {
 
@@ -9,11 +10,18 @@ const Intro = props => {
   };
 
   return(
-    <Flex direction="column" align="center" >
-      <Heading fontSize="2xl">Hey, I'm Liwei Chen</Heading>
-      <Text display={{base: "inherit", md: "none"}} fontSize="lg" mt="10px" >A Frontend focused Web Developer</Text>
-      <Text display={{base: "none", md: "inherit"}} fontSize="lg" mt="20px" w="700px" textAlign="center" >A Frontend focused Web Developer building the Frontend of Websites and Web Applications that lead to the success of the overall product</Text>
-      <Button onClick={toggleWorks} w={{base: "100px", md: "200px"}} mt={{base: "20px", md: "40px"}} _focus={{border: "none"}} _hover={{bg: "rgba(156, 168, 184, 1)"}} _active={{bg: "rgba(134, 150, 167, 1)"}} bg="rgba(193, 203, 215, 1)">Works</Button>
+    <Flex direction="column" align="center">
+      <Box display={{base: 'none', '2xl': 'inherit'}} position='absolute' top='0' right='0' filter={useColorModeValue('brightness(80%)', 'brightness(30%)')}>
+        <video autoPlay loop muted>
+          <source src='bgVideo.mp4' type="video/mp4" />
+        </video>
+      </Box>
+      <Flex direction='column' align='center' zIndex='99'>
+        <Heading fontSize="2xl" color={{'2xl': 'white'}}>Hey, I'm Liwei Chen</Heading>
+        <Text display={{base: "inherit", md: "none"}} fontSize="lg" mt="10px" >A Frontend focused Web Developer</Text>
+        <Text display={{base: "none", md: "inherit"}} fontSize="lg" mt="20px" w="700px" textAlign="center" color={{'2xl': 'white'}} >A Frontend focused Web Developer building the Frontend of Websites and Web Applications that lead to the success of the overall product</Text>
+        <Button onClick={toggleWorks} w={{base: "100px", md: "200px"}} mt={{base: "20px", md: "40px"}} _focus={{border: "none"}} _hover={{bg: "rgba(156, 168, 184, 1)"}} _active={{bg: "rgba(134, 150, 167, 1)"}} bg={useColorModeValue("rgba(193, 203, 215, 1)", "#656176")}>Works</Button>
+      </Flex>
     </Flex>
   )
 }
