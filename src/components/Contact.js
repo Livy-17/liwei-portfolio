b7f48d97047aa73adacb14c3113aec3de61c4843
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-04 14:39:30 +0100

    1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,11 @@ import { Box, Flex, Heading, Text, Input, Textarea, Button, FormControl, FormLab
 const Contact = props => {
   return (
     <Flex id='contactSection' direction="column" align="center" p={{base: "60px 0 60px 0", md: "100px 0 100px 0"}}>
-      <Heading mb="40px" fontSize="2xl">CONTACT</Heading>
-      <Text fontSize="lg" mb="50px" w={{base: "300px", md: "760px"}} >&nbsp;&nbsp;&nbsp;&nbsp;Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</Text>
-      <Box border="solid 1px gray" borderRadius="5px" p="30px 20px 30px 20px" w={{base: "300px", md: "760px"}} >
+      <Flex direction='column' align='center' justifyContent='center' data-aos='zoom-in-up'>
+        <Heading mb="40px" fontSize="2xl">CONTACT</Heading>
+        <Text fontSize="lg" mb="50px" w={{base: "300px", md: "760px"}} >&nbsp;&nbsp;&nbsp;&nbsp;Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</Text>
+      </Flex>
+      <Box data-aos='zoom-in' border="solid 1px gray" borderRadius="5px" p="30px 20px 30px 20px" w={{base: "300px", md: "760px"}} >
         <FormControl>
           <FormLabel htmlFor="name" mb="20px">Name</FormLabel>
           <Input id="name" mb="20px" placeholder="Enter your name" />
--- src/components/Works.js
+++ src/components/Works.js
@@ -5,33 +5,49 @@ import Work from "./Work";
 const Works = props => {
   return (
     <Flex id='worksSection' direction="column" align="center" pt={{base: "60px", md: "100px"}} pb={{base: "20px", md: "60px"}} bg={useColorModeValue('#F7F6F2','#1B2430')}>
-      <Heading mb="40px" fontSize="2xl">Works</Heading>
-      <Text fontSize="lg" mb="50px" w={{base: "300px", md: "760px"}}>&nbsp;&nbsp;&nbsp;&nbsp;Here you will find some of the personal and clients projects that I created with each project containing its own case study</Text>
-      <Work 
-      theCount="1"
-      theHeading="A Heading"
-      theText="Some description about the workfdsfhiuewhfioudhcjsiunvusdhiufhsuihdfiushduihfiushdiuhfuishduifhiusdhui sudhfiushdiuhfuhishfiud"
-      />
-      <Work
-      theCount="2"
-      theHeading="A Heading"
-      theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
-      />
-      <Work
-      theCount="3"
-      theHeading="A Heading"
-      theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
-      />
-      <Work
-      theCount="4"
-      theHeading="A Heading"
-      theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
-      />
-      <Work
-      theCount="5"
-      theHeading="A Heading"
-      theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
-      /> 
+      <Flex data-aos='flip-down' direction='column' align='center' justifyContent='center'>
+        <Heading mb="40px" fontSize="2xl">Works</Heading>
+        <Text fontSize="lg" mb="50px" w={{base: "300px", md: "760px"}}>&nbsp;&nbsp;&nbsp;&nbsp;Here you will find some of the personal and clients projects that I created with each project containing its own case study</Text>
+      </Flex>
+      <Box data-aos='flip-left'>
+        <Work 
+        theCount="1"
+        theHeading="A Heading"
+        theText="Some description about the workfdsfhiuewhfioudhcjsiunvusdhiufhsuihdfiushduihfiushdiuhfuishduifhiusdhui sudhfiushdiuhfuhishfiud"
+        />
+      </Box>
+      <Box data-aos='flip-right'>
+        <Work
+        data-aos='flip-right'
+        theCount="2"
+        theHeading="A Heading"
+        theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
+        />
+      </Box>
+      <Box data-aos='flip-left'>
+        <Work
+        data-aos='flip-left'
+        theCount="3"
+        theHeading="A Heading"
+        theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
+        />
+      </Box>
+      <Box data-aos='flip-right'>
+        <Work
+        data-aos='flip-right'
+        theCount="4"
+        theHeading="A Heading"
+        theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
+        />
+      </Box>
+      <Box data-aos='flip-left'>
+        <Work
+        data-aos='flip-left'
+        theCount="5"
+        theHeading="A Heading"
+        theText="bjlajfidjfiojsidoajfiodjsiodfjviosjoijvoisjojdsiojfio"
+        /> 
+      </Box>
     </Flex>
   )
 }
