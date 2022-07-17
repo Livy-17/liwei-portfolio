1d7ddec7766e800ab354d9afe1e0af80e500b990
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-17 23:22:14 +0100

    1

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="https://www.liweichen.info/" element={
+        <Route path={`${process.env.PUBLIC_URL}/`} element={
           <Box w='100vw' bg={useColorModeValue('', '')}>
             <Navbar />
             <Flex direction="column" h="100vh" w='100%' justifyContent="center" alignItems='center' bg={useColorModeValue('#F7F6F2','#1B2430')}>
@@ -31,7 +31,7 @@ const App = () => {
             <Footer />
           </Box>
         } />
-        <Route path='https://www.liweichen.info/submitted' element={
+        <Route path={`${process.env.PUBLIC_URL}/submitted`} element={
           <Submitted />
         } />
       </Routes>
