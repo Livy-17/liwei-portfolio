58c536fc9d1cadfe9ffe435eaf118f629b2eeab8
Liwei <42135339+Livy-17@users.noreply.github.com>
2022-08-02 20:54:33 +0100

    Contact Bug

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = props => {
         <Heading mb="40px" fontSize="2xl">CONTACT</Heading>
         <Text fontSize="lg" mb="50px" w={{base: "300px", md: "700px", lg: "800px"}}>&nbsp;&nbsp;&nbsp;&nbsp;Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</Text>
       </Flex>
-      <Box data-aos='zoom-in' border="solid 1px gray" borderRadius="5px" p="30px 20px 30px 20px" w={{base: "300px", md: "700px", lg: "800px"}}>
+      <Box border="solid 1px gray" borderRadius="5px" p="30px 20px 30px 20px" w={{base: "300px", md: "700px", lg: "800px"}}>
         <form action="https://formsubmit.co/f7cd069ea5d53a48b1364bc2c8c49b9c" method="POST">
           <label htmlFor='name' style={{ display: 'block', fontWeight: 'bold', marginBottom: '20px' }}>Name</label>
           <input type="text" name="name" id='name' placeholder="" required style={{ display: 'block', marginBottom: '20px', border: 'solid 1px lightgray', width: '100%', height: '40px', borderRadius: '5px', padding: '3px 0 0 15px' }} />
